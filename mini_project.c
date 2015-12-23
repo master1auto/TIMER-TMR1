ef9d8636638d75911a0ce0fe1f9fc57a94d377f4
Medab-Malek <medab-malek@hotmail.com>
2015-12-23 17:30:59 +0100

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -1,4 +1,11 @@
-#include<stdio.h>
-void main() {
-  printf("Hello World");
-}
+TestReflexe:
+ Clr PortC:#bLed
+ Call Attente ;attente15secondes
+ Set PortC:#bLed
+ Clr PortD
+AtPous: Call Del20ms
+ Inc PortD;Duréeenmultiplede20ms
+ TestSkip,BSPortC:#bPous
+ Jump AtPous
+Fini:Jump Fini ;JumpAPCidem,carAPC=adressedel’instruction
+
