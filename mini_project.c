6dbbba15a95f5c219cd274bcdcaf032f08613d33
elmoundher <elmoundher@gmail.com>
2015-12-27 00:14:26 +0100

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -1,3 +1,15 @@
+void  TIMER1_isr(){
+set_timer1(0x0BDC);
+}
 
+void (){
+     set_tris_b(0b01111111);
+     RB7=0;
+     setup_timer_1(T1_INTERNAL|T1_DIV_BY_8);
+     enable_interrupts(INT_TIMER1);
+     enable_interrupts(GLOBAL);
+     set_timer1(0x0BDC);
+     while(true);
+}
 
 
