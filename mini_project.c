da3173dcc723b91619f58b909fd74f8b0bde6099
RiadMicroP <bassaUKMO@gmail.com>
2016-01-02 00:53:56 -0800

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -1,12 +1,19 @@
 void timer1() {
-	TMR1H = 0;	// clear timer1
-	TMR1L = 0;	// before enabling timer 1
-	T1CON = 0x01;   // timer 1 configuration
-}            
-
-
+    int deley;
+	TMR1H = 0;
+	TMR1L = 0;
+	T1CON = 0x01;
+	deley(30);
+	//deley = /2 ; // we fine a problem over here
+}
 void main() {
 	TRISB = 0;
 	timer1();
+int c, n, fact = 1;
+    printf("Enter a number to calculate it's factorial\n");
+    scanf("%d", &n);
+    for (c = 1; c <= n; c++){
+        fact = fact * c;
+        printf("Factorial of %d = %d\n", n, fact);}
 	while(1){}
 }
