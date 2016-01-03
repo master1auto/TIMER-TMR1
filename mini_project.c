df4b79008058c66814ed4c828f5b179fac5fa5c9
RiadMicroP <bassaUKMO@gmail.com>
2016-01-03 11:19:57 -0800

    Update mini_project.c
    Please sir is there any missing instruction in this code ?

--- mini_project.c
+++ mini_project.c
@@ -1,20 +1,19 @@
 void timer1() {
-    int deley;
-	TMR1H = 0;
-	TMR1L = 0;
-	T1CON = 0x01;
-	TMR1ON = 1;
-	deley(30);
-	//deley = /2 ; // we fine a problem over here
-}
+	PIR1.TMR1IF = 0;       // Reset the TMR1IF flag bit
+	TMR1H = 0x22;          // Set initial value for the timer TMR1
+	TMR1L = 0x00;
+	TMR1CS = 0;            // Timer1 counts pulses from internal oscillator
+	T1CKPS1 = T1CKPS0 = 1; // Assigned prescaler rate is 1:8
+	PIE1.TMR1IE = 1;       // Enable interrupt on overflow
+	INTCON = 0xC0;         // Enable interrupt (bits GIE and PEIE)
+	TMR1ON_bit = 1;        // Start the timer 
+	}
 void main() {
-	TRISB = 0;
-	timer1();
-int c, n, fact = 1;
-    printf("Enter a number to calculate it's factorial\n");
-    scanf("%d", &n);
-    for (c = 1; c <= n; c++){
+	int c, n, fact = 1;	// Declaration Variable 
+	TRISB = 0;		// Using port B as an out
+	timer1();		// Using timer1 function 
+    for (c = 1; c <= n; c++){   // In order to calculate a vector
         fact = fact * c;
-        printf("Factorial of %d = %d\n", n, fact);}
+        TMR1ON_bit = 0;		// Stop the timer
 	while(1){}
-}
+	}
